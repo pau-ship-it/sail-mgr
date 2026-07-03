8dc569d31cde2e88fb764e497fc9ef306d5b734a
pau-ship-it <pau@onepalma.net>
2026-07-03 07:36:59 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SAIL_CONFIG = {
-  GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
-  CEO_EMAIL: '',  pau@onepalma.net
+  GOOGLE_CLIENT_ID: 'pau@onepalma.com',
+  CEO_EMAIL: '',
   FIREBASE: {
     apiKey: "AIzaSyDclRSyehYiFGQtnXs0DEOE1F5MxxKKQlU",
     authDomain: "sail-mgr.firebaseapp.com",
